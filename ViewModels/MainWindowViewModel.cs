9e8d82805b97c96db83c4d829d7b7ae31c27905b
QuangHaii <hainoob2002@gmail.com>
2024-06-27 15:57:38 +0700

    Hai test push

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,7 +3,7 @@
     public class MainWindowViewModel : ViewModelBase
     {
 #pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
+        public string Greeting => "Hello 123!";
 #pragma warning restore CA1822 // Mark members as static
     }
 }
