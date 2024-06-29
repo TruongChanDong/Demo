8d206c669119f2b5c2169fb870cf45c6da151490
ChanDong12 <dongtruong328@gmail.com>
2024-06-29 14:04:57 +0700

    Login

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
 
 namespace Demo.Views
 {
@@ -7,6 +10,35 @@ namespace Demo.Views
         public MainWindow()
         {
             InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var username = this.FindControl<TextBox>("Username").Text;
+                var password = this.FindControl<TextBox>("Password").Text;
+
+                Console.WriteLine($"Username: {username}, Password: {password}");
+
+                if (username == "admin" && password == "admin")
+                {
+                    this.FindControl<TextBlock>("Message").Text = "Login Successful!";
+                    Console.WriteLine("Login Successful!");
+                }
+                else
+                {
+                    this.FindControl<TextBlock>("Message").Text = "Invalid username or password.";
+                    Console.WriteLine("Invalid username or password.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
