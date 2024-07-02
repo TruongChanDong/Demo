4f82478651d5f7c43ff3cc7b64635086db777cce
QuangHaii <hainoob2002@gmail.com>
2024-07-02 16:43:26 +0700

    Update GET method demo

---
+++ ViewModels/ListViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Demo.ViewModels
+{
+    public partial class ListViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        public ObservableCollection<User> _userList = new();
+
+        [ObservableProperty]
+        public string? _message;
+
+        private static HttpClient httpClient = new()
+        {
+            BaseAddress = new Uri("http://localhost:8080/"),
+        };
+
+        [RelayCommand]
+        public async Task GetListItem()
+        {
+            var users = new List<User>();
+            try
+            {
+                users = await httpClient.GetFromJsonAsync<List<User>>("user");
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+                users = null;
+            }
+            if (users != null)
+            {
+                UserList = new ObservableCollection<User>(users);
+                Message = $"Succesfully get {users.Count} users !!";
+            }
+        }
+
+    }
+}
---
+++ Views/ListView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Demo.Views
+{
+    public partial class ListView : UserControl
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
