98f8aa3e82885228d7ff80edbd087c51347344f8
QuangHaii <hainoob2002@gmail.com>
2024-07-08 15:38:35 +0700

    Update test chart

---
+++ Views/RealTimeChartView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Demo.Views
+{
+    public partial class RealTimeChartView : UserControl
+    {
+        public RealTimeChartView()
+        {
+            InitializeComponent();
+        }
+    }
+}
